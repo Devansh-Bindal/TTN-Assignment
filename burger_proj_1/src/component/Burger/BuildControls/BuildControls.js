f0b89df27f640c7c6660b2a1ec1d1035c23c3327
Devansh-Bindal <devansh.bindal@tothenew.com>
2020-03-27 17:31:58 +0530

    Css Issues

---
+++ burger_proj_1/src/component/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuilControl';
+import classes from './BuildControl/BuildControl.module.css';
+const controls=[
+    {label:'salad' ,type:'salad'},
+    {label:'meat' ,type:'meat'},
+    {label:'bacon' ,type:'bacon'},
+    {label:'cheese' ,type:'cheese'}
+]
+const BuildControls=()=>(
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl=>(
+          <BuildControl key={ctrl.label} label={ctrl.label}/>
+      ))}
+    </div>
+);
+export default BuildControls;
