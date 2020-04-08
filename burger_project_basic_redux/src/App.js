6aacf854c1d66b89b6bf727c110c0bd9864af651
Devansh-Bindal <devansh.bindal@tothenew.com>
2020-04-08 10:09:30 +0530

    Basic redux

---
+++ burger_project_basic_redux/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ContactData from '../src/containers/Checkout/ContactData/ContactData';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout"exact component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Route path='/checkout/contact-data' component={ContactData} />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
